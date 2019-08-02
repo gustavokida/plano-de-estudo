026f063a71f06a3d0d23d5f5e136647a71d9ee97
gustavokida <gustavokida@live.com>
2019-08-02 16:30:13 -0400

    Criado entidades conteudo e materia

---
+++ plano-de-estudo-domain/src/plano/de/estudo/domain/entidades/Conteudo.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plano.de.estudo.domain.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Conteudo {
+    private int id;
+    private String nome;
+    private String observacao;
+    private Date data;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+    
+    
+
+    
+}
---
+++ plano-de-estudo-domain/src/plano/de/estudo/domain/entidades/Materia.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plano.de.estudo.domain.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Materia {
+    private int id;
+    private String nome;
+    private Date horario;
+    private int cargahoraria;
+    private float media;
+    private boolean aprovado;
+    private String observacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+
+    public int getCargahoraria() {
+        return cargahoraria;
+    }
+
+    public void setCargahoraria(int cargahoraria) {
+        this.cargahoraria = cargahoraria;
+    }
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+    
+}
