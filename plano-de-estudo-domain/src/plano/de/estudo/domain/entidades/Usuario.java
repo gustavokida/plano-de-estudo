47cb5b650451a06cb0ad3449b0bc5aa0e1e50c48
gustavokida <gustavokida@live.com>
2019-08-02 15:52:38 -0400

    Criado entidade usuario

---
+++ plano-de-estudo-domain/src/plano/de/estudo/domain/entidades/Usuario.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plano.de.estudo.domain.entidades;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String celular;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    
+    
+}
